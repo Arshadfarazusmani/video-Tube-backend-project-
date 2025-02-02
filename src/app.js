5adde05ccbcf5333cf1910b885570106faa28f4a
Arshad Faraz <arshadfarazusmani@gmail.com>
2025-02-02 22:53:48 +0530

    add express app and docs to code base

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import express from "express"; // import express module to create express application
+const app = express(); // create an instance of an Express application
+
+export { app }; // export the app instance

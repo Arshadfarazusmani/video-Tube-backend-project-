bdc9ce3569011a3a063d4f8a27a89dbf676b71a4
Arshad Faraz <arshadfarazusmani@gmail.com>
2025-02-02 23:36:54 +0530

    add middleware configurations for CORS, JSON, URL-encoded, static files, and cookie parsing

--- src/app.js
+++ src/app.js
@@ -1,4 +1,31 @@
 import express from "express"; // import express module to create express application
 const app = express(); // create an instance of an Express application
 
-export { app }; // export the app instance
+//  configuring app to use cors middleware.
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+    
+}));
+// configuring app to use json middleware.
+app.use(express.json({
+    limit: '16kb'
+}));
+// configuring app to use urlencoded middleware.
+app.use(express.urlencoded({
+    extended: true,
+    limit: '16kb'
+}));
+// configuring app to use static middleware.
+// serve static files from the 'public' directory.
+
+app.use(express.static('public'));
+
+
+// configuring app to use cookie-parser middleware.
+// cookie-parser is used to parse cookies attached to the client request object.
+
+app.use(cookieParser());
+
+export  {app}; // export the app object to be used in other modules.
